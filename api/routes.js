2d36e73b4ad97ffaf4b9c18180aa7e78d5a3bc74
romainniamor <romainnavoret@gmail.com>
2024-03-26 16:05:14 +0100

    🔧

--- api/routes.js
+++ api/routes.js
@@ -20,43 +20,43 @@ router.get("/japan-class", (req, res) => {
   res.json(success(message, fakeData));
 });
 
-// router.post("/japan-class/message", async (req, res) => {
-//   const userMessage = req.body.message;
-//   if (
-//     !userMessage ||
-//     typeof userMessage !== "string" ||
-//     userMessage.trim().length === 0
-//   ) {
-//     return res.status(400).json({ error: "error message" });
-//   }
-//   try {
-//     const completion = await openai.chat.completions.create({
-//       model: "gpt-3.5-turbo",
+router.post("/japan-class/message", async (req, res) => {
+  const userMessage = req.body.message;
+  if (
+    !userMessage ||
+    typeof userMessage !== "string" ||
+    userMessage.trim().length === 0
+  ) {
+    return res.status(400).json({ error: "error message" });
+  }
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
 
-//       messages: [
-//         {
-//           role: "system",
-//           content: prompt,
-//         },
-//         {
-//           role: "user",
-//           content: `How to say in Japanes ${userMessage}`,
-//         },
-//       ],
-//     });
-//     if (
-//       !completion.choices ||
-//       completion.choices.length === 0 ||
-//       !completion.choices[0].message ||
-//       !completion.choices[0].message.content
-//     ) {
-//       return res.status(500).json({ error: "Unable to translate message" });
-//     }
-//     let messages = JSON.parse(completion.choices[0].message.content);
-//     res.send(messages);
-//   } catch (error) {
-//     res.status(500).json({ error: "OPENAI API ERROR" });
-//   }
-// });
+      messages: [
+        {
+          role: "system",
+          content: prompt,
+        },
+        {
+          role: "user",
+          content: `How to say in Japanes ${userMessage}`,
+        },
+      ],
+    });
+    if (
+      !completion.choices ||
+      completion.choices.length === 0 ||
+      !completion.choices[0].message ||
+      !completion.choices[0].message.content
+    ) {
+      return res.status(500).json({ error: "Unable to translate message" });
+    }
+    let messages = JSON.parse(completion.choices[0].message.content);
+    res.send(messages);
+  } catch (error) {
+    res.status(500).json({ error: "OPENAI API ERROR" });
+  }
+});
 
 export default router;
