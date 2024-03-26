9c99eeeec21836881b48dbf39ab0036ed027d96d
romainniamor <romainnavoret@gmail.com>
2024-03-26 15:29:34 +0100

    🔧 add openai

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
 import { fakeData } from "../data/initMessage.js";
 import { success } from "../helper/success.js";
+import OpenAI from "openai";
+import { prompt } from "../enums/prompt.js";
 
+dotenv.config();
 const router = express.Router();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 router.get("/", (req, res) => {
   res.json("Backend");
@@ -11,6 +18,45 @@ router.get("/", (req, res) => {
 router.get("/japan-class", (req, res) => {
   const message = "Fake data for Japan class";
   res.json(success(message, fakeData));
+});
+
+router.post("/japan-class/message", async (req, res) => {
+  const userMessage = req.body.message;
+  if (
+    !userMessage ||
+    typeof userMessage !== "string" ||
+    userMessage.trim().length === 0
+  ) {
+    return res.status(400).json({ error: "error message" });
+  }
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+
+      messages: [
+        {
+          role: "system",
+          content: prompt,
+        },
+        {
+          role: "user",
+          content: `How to say in Japanes ${userMessage}`,
+        },
+      ],
+    });
+    if (
+      !completion.choices ||
+      completion.choices.length === 0 ||
+      !completion.choices[0].message ||
+      !completion.choices[0].message.content
+    ) {
+      return res.status(500).json({ error: "Unable to translate message" });
+    }
+    let messages = JSON.parse(completion.choices[0].message.content);
+    res.send(messages);
+  } catch (error) {
+    res.status(500).json({ error: "OPENAI API ERROR" });
+  }
 });
 
 export default router;
