d08ae39310cbc1590bdad349d1a9f8f348f859de
romainniamor <romainnavoret@gmail.com>
2024-03-26 15:35:14 +0100

    🔧 add prompt openai

---
+++ enums/prompt.js
@@ -0,0 +1,4 @@
+import { fakeData } from "../data/initMessage.js";
+
+export const prompt = `You are a Japanese language teacher. Your student asks you how to say something from english to japanese.
+You should respond like this: ${JSON.stringify(fakeData)}`;
